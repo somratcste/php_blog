7622ce3fce217314d106c9bba8ee06fbb7dd3191
unknown <somratcste@SOMRAT.(none)>
2015-10-11 20:52:41 +0600

    commenting system that's approve by admin

--- admin/comment-approve.php
+++ admin/comment-approve.php
@@ -89,7 +89,99 @@ if(isset($success_message)) {echo "<div class='success'>".$success_message."</di
 		<td><?php echo $row['c_email']; ?></td>
 		<td><?php echo $row['c_url']; ?></td>
 		<td><?php echo $row['c_message']; ?></td>
-		<td><?php echo $row['post_id']; ?></td>
+		<td>
+
+		<a class="fancybox" href="#inline<?php echo $i; ?>"><?php echo $row['post_id']; ?></a>
+		<div id="inline<?php echo $i; ?>" style="width:700px;display: none;">
+
+		<h3 style="border-bottom:2px solid blue; margin-top:10px;">View Post Details</h3>
+		<p>
+			
+
+			<?php
+			$statement1 = $db->prepare("SELECT * FROM tbl_posts WHERE post_id = ?");
+			$statement1->execute(array($row['post_id']));
+			$result1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
+			foreach($result1 as $row1)
+			{
+				?>
+
+            <table>
+			<tr>
+					<td><b>Title Name</b></td>
+				</tr>
+				<tr>
+					<td><?php echo $row1['post_title']; ?></td>
+				</tr>
+				<tr>
+					<td><b>Description</b></td>
+				</tr>
+				<tr>
+					<td><?php echo $row1['post_description']; ?></td>
+				</tr>
+				<tr>
+					<td><b>Featured Image</b></td>
+				</tr>
+				<tr>
+					<td><img src="../uploads/<?php echo $row1['post_image']; ?>" alt="" width = "200px"></td>
+				</tr>
+				<tr>
+					<td><b>Category Name</b></td>
+				</tr>
+				<tr>
+					<td>
+						<?php
+						$statement2 = $db->prepare("SELECT * FROM tbl_categories WHERE cat_id=?");
+						$statement2->execute(array($row1['cat_id']));
+						$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+						foreach($result2 as $row2)
+						{
+							echo $row2['cat_name'];
+						}
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td><b>Tag Name</b></td>
+				</tr>
+				<tr>
+					<td>
+						<?php
+						$arr = explode(",",$row1['tag_id']);
+						$count_arr = count(explode(",",$row1['tag_id']));
+						$k=0;
+						for($j=0;$j<$count_arr;$j++)
+						{
+							
+							$statement2 = $db->prepare("SELECT * FROM tbl_tags WHERE tag_id=?");
+							$statement2->execute(array($arr[$j]));
+							$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+							foreach($result2 as $row2)
+							{
+								$arr1[$k] = $row2['tag_name'];
+							}
+							$k++;
+						}
+						$tag_names = implode(",",$arr1);
+						echo $tag_names;
+						?>
+					</td>
+				</tr>
+			</table>
+
+		</p>
+		</div>
+				<?php
+
+			}
+			?>
+
+				
+							
+
+
+
+		</td>
 		<td><a href="comment-approve.php?id=<?php echo $row['c_id']; ?>">Approved</td></a>
 
 		</tr>
@@ -114,7 +206,7 @@ if(isset($success_message)) {echo "<div class='success'>".$success_message."</di
 	</tr>
 	
 	<?php
-	$i=0;
+	$n=0;
 	$statement = $db->prepare("SELECT * FROM tbl_comments WHERE active = 1 ORDER BY c_id DESC");
 	$statement->execute();
 	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -123,12 +215,107 @@ if(isset($success_message)) {echo "<div class='success'>".$success_message."</di
 		$i++;
 	?>
 		<tr>
-		<td><?php echo $i; ?></td>
+		<td><?php echo $n; ?></td>
 		<td><?php echo $row['c_name']; ?></td>
 		<td><?php echo $row['c_email']; ?></td>
 		<td><?php echo $row['c_url']; ?></td>
 		<td><?php echo $row['c_message']; ?></td>
-		<td><?php echo $row['post_id']; ?></td>
+		<td>
+
+
+	    <a class="fancybox" href="#inline<?php echo $i; ?>"><?php echo $row['post_id']; ?></a>
+		<div id="inline<?php echo $i; ?>" style="width:700px;display: none;">
+
+		<h3 style="border-bottom:2px solid blue; margin-top:10px;">View Post Details</h3>
+		<p>
+			
+
+			<?php
+			$statement1 = $db->prepare("SELECT * FROM tbl_posts WHERE post_id = ?");
+			$statement1->execute(array($row['post_id']));
+			$result1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
+			foreach($result1 as $row1)
+			{
+				?>
+
+            <table>
+			<tr>
+					<td><b>Title Name</b></td>
+				</tr>
+				<tr>
+					<td><?php echo $row1['post_title']; ?></td>
+				</tr>
+				<tr>
+					<td><b>Description</b></td>
+				</tr>
+				<tr>
+					<td><?php echo $row1['post_description']; ?></td>
+				</tr>
+				<tr>
+					<td><b>Featured Image</b></td>
+				</tr>
+				<tr>
+					<td><img src="../uploads/<?php echo $row1['post_image']; ?>" alt="" width = "200px"></td>
+				</tr>
+				<tr>
+					<td><b>Category Name</b></td>
+				</tr>
+				<tr>
+					<td>
+						<?php
+						$statement2 = $db->prepare("SELECT * FROM tbl_categories WHERE cat_id=?");
+						$statement2->execute(array($row1['cat_id']));
+						$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+						foreach($result2 as $row2)
+						{
+							echo $row2['cat_name'];
+						}
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td><b>Tag Name</b></td>
+				</tr>
+				<tr>
+					<td>
+						<?php
+						$arr = explode(",",$row1['tag_id']);
+						$count_arr = count(explode(",",$row1['tag_id']));
+						$k=0;
+						for($j=0;$j<$count_arr;$j++)
+						{
+							
+							$statement2 = $db->prepare("SELECT * FROM tbl_tags WHERE tag_id=?");
+							$statement2->execute(array($arr[$j]));
+							$result2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+							foreach($result2 as $row2)
+							{
+								$arr1[$k] = $row2['tag_name'];
+							}
+							$k++;
+						}
+						$tag_names = implode(",",$arr1);
+						echo $tag_names;
+						?>
+					</td>
+				</tr>
+			</table>
+
+     	</p>
+     	</div>
+				<?php
+
+			}
+			?>
+
+				
+							
+	
+
+
+
+		
+		</td>
 		<td><a href="comment-approve.php?cid=<?php echo $row['c_id']; ?>">Unapproved</td></a>
 
 		</tr>
@@ -136,7 +323,6 @@ if(isset($success_message)) {echo "<div class='success'>".$success_message."</di
 	}	
 	 ?>
 </table>
-
 			
 
 <?php 
