ad316dd583f574a790202898e195292cc527b61e
unknown <somratcste@SOMRAT.(none)>
2015-10-12 21:04:36 +0600

    password changed link are added

--- admin/index.php
+++ admin/index.php
@@ -6,16 +6,11 @@ if($_SESSION['name']!='admin')
 	header('location: login.php');
 }
 ?>
-<?php
-include ('header.php');
-?>
-			<h2>Welcome to the Admin Panel</h2>
-			<div id="content_info">
-				WELCOME TO THE DASHBOARD <br>
-				SAMPLE BLOG WITH PHP
-			</div>
-
-<?php 
-include('footer.php');
-?>
+<?php include("header.php"); ?>
+<h2> Welcome to Admin Panel</h2>
+<div style="font-weight:bold;color:#3d9ccd;font-size:28px;text-align:center;padding-top:50px;">
+	Welcome to the dashboard of <br>
+	Sample Blog with PHP
+</div>
+<?php include("footer.php"); ?>			
 		
